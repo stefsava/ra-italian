32735933e10f139582399ef3280a7db12ee2fe73
Stefano Savanelli <s.savanelli@dagilis.com>
2021-03-05 11:24:54 +0100

    remove deprecate ra-tree module

--- packages/ra-tree-language-italian/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  ra: {
-    tree: {
-      drag_preview:
-                'Nodo #%{id} |||| Nodo #%{id} con %{smart_count} figli',
-      root_target: 'Trascina un nodo qui per renderlo nodo principale'
-    }
-  }
-}
